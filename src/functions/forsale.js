3b24ae435af7494517c63bd610f976a3c930f9cf
Felipe Oliveira <felipeoes@usp.br>
2021-10-30 21:03:49 -0300

    merging branches

--- src/functions/forsale.js
+++ src/functions/forsale.js
@@ -1,9 +1,9 @@
-const db = require("../../db_us_estate.json");
+const db = require('../../db_us_estate.json');
 
 function getHousesForSaleList() {
   var housesForSale = [];
 
-  db.data.home_search.results.forEach(value => {
+  db.data.home_search.results.forEach((value) => {
     if (value.status === 'for_sale') {
       housesForSale.push(value);
     }
