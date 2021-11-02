14aee341db10a20ad1894e7ea48d8fe6b421318b
Guilherme Jimenes <guilherme.jimenes@usp.br>
2021-11-02 20:48:11 -0300

    Melhoria na precisão do filtro for rent

--- src/functions/forrent.js
+++ src/functions/forrent.js
@@ -12,34 +12,24 @@ function getHousesForRentList(params) {
 
   // quartos
   if (Number(params[3]) === 0) {
-    result = result.filter((obj) => obj.description.beds >= 0);
+    result = result.filter((obj) => obj.description.beds_max >= 0);
   } else {
-    result = result.filter((obj) => obj.description.beds <= Number(params[3]));
+    result = result.filter((obj) => obj.description.beds_max == Number(params[3]));
   }
 
   console.log('Qtde quartos', result.length);
 
   // banheiros
   if (Number(params[4]) === 0) {
-    result = result.filter((obj) => obj.description.baths >= 0);
+    result = result.filter((obj) => obj.description.baths_max >= 0);
   } else {
     result = result.filter(
-      (obj) => obj.description.baths <= Number(params[4])
+      (obj) => obj.description.baths_max == Number(params[4])
     );
   }
 
   console.log('Qtdes banheiros', result.length);
 
-  // garagem
-  if (Number(params[5]) === 0) {
-    result = result.filter((obj) => obj.description.garage >= 0);
-  } else {
-    result = result.filter(
-      (obj) => obj.description.garage <= Number(params[5])
-    );
-  }
-
-  console.log('Qtde vagas carro', result.length);
 
   return result;
 }
